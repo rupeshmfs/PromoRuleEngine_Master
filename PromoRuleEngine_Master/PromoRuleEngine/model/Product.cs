6c0f6ced1f871cb00aa9832af46b3be518faaf0b
rupeshmfs <rupeshkumar.nayak@gmail.com>
2020-06-07 15:26:23 +0530

    Product class added

---
+++ PromoRuleEngine_Master/PromoRuleEngine/model/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromoRuleEngine_Master.model
+{
+    public class Product
+    {
+        public string ID { get; set; }
+        public decimal Price { get; set; }
+    }
+}
