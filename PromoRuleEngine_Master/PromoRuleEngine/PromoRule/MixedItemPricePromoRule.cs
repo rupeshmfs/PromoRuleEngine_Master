98cf08b64c7f7b01c209cb0bad6d545d2ff13620
rupeshmfs <rupeshkumar.nayak@gmail.com>
2020-06-07 16:20:35 +0530

    Added MixedItemPricePromoRule class

---
+++ PromoRuleEngine_Master/PromoRuleEngine/PromoRule/MixedItemPricePromoRule.cs
@@ -0,0 +1,18 @@
+﻿using PromoRuleEngine_Master.Interface;
+using PromoRuleEngine_Master.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromoRuleEngine_Master.PromoRule
+{
+    class MixedItemPricePromoRule : ICalculatePromoPrice
+    {
+        public decimal CalculatePromoPrice(ShoppingCart cart)
+        {
+            return 0.0m;
+        }
+    }
+}
