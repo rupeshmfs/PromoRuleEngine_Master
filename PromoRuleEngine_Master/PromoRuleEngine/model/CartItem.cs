4d1b4dcaf3de98eb7d2a863cc5c07ad72eb4db13
rupeshmfs <rupeshkumar.nayak@gmail.com>
2020-06-07 15:35:54 +0530

    Interface, Model added

---
+++ PromoRuleEngine_Master/PromoRuleEngine/model/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromoRuleEngine_Master.model
+{
+    public class CartItem
+    {
+        public Product Item { get; set; }
+        public int TotalItem { get; set; }
+        public bool IsRuleApplied { get; set; }
+    }
+}
---
+++ PromoRuleEngine_Master/PromoRuleEngine/model/ShoppingCart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromoRuleEngine_Master.model
+{
+    public class ShoppingCart
+    {
+        public IEnumerable<CartItem> CartItems { get; set; }
+        public Decimal TotalPrice { get; set; }
+    }
+}
