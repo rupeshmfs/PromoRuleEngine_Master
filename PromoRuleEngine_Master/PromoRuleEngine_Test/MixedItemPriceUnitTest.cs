846a7d331ca7781a22fd52eac388de36e538de56
rupeshmfs <rupeshkumar.nayak@gmail.com>
2020-06-07 16:51:20 +0530

    added the test case

--- PromoRuleEngine_Master/PromoRuleEngine_Test/MixedItemPriceUnitTest.cs
+++ PromoRuleEngine_Master/PromoRuleEngine_Test/MixedItemPriceUnitTest.cs
@@ -1,5 +1,7 @@
-﻿using PromoRuleEngine_Master;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PromoRuleEngine_Master;
 using PromoRuleEngine_Master.Interface;
+using PromoRuleEngine_Master.model;
 using PromoRuleEngine_Master.PromoRule;
 using System;
 using System.Collections.Generic;
@@ -21,6 +23,23 @@ namespace PromoRuleEngine_Test
             _promoRules.Add(new FixedPricePromoRule("B", 2, 45));
             _promoRules.Add(new MixedItemPricePromoRule(new List<string> { "C", "D" }, 30, 2));
             _priceCalculator = new PriceCalculatorEngine(_promoRules);
+        }
+
+        [TestMethod]
+        public void MixedItemPromoPriceUnitTest()
+        {
+            ShoppingCart shopingCart = new ShoppingCart();
+            List<CartItem> cartItems = new List<CartItem>();
+
+            cartItems.Add(new CartItem() { TotalProduct = 3, Product = new Product() { ID = "A", Price = 50 } });
+            cartItems.Add(new CartItem() { TotalProduct = 5, Product = new Product() { ID = "B", Price = 30 } });
+            cartItems.Add(new CartItem() { TotalProduct = 1, Product = new Product() { ID = "C", Price = 20 } });
+            cartItems.Add(new CartItem() { TotalProduct = 1, Product = new Product() { ID = "D", Price = 15 } });
+            shopingCart.CartItems = cartItems;
+
+            decimal TotalPrice = _priceCalculator.TotalPriceCalculation(shopingCart);
+
+            Assert.AreEqual(280.00m, TotalPrice);
         }
     }
 }
