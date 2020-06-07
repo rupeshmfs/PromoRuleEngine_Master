d4764efe78d2f116191a6841e585c560f6c01812
rupeshmfs <rupeshkumar.nayak@gmail.com>
2020-06-07 16:23:47 +0530

    Added MixedItemPricePromoRule

--- PromoRuleEngine_Master/PromoRuleEngine/PromoRule/MixedItemPricePromoRule.cs
+++ PromoRuleEngine_Master/PromoRuleEngine/PromoRule/MixedItemPricePromoRule.cs
@@ -10,9 +10,28 @@ namespace PromoRuleEngine_Master.PromoRule
 {
     class MixedItemPricePromoRule : ICalculatePromoPrice
     {
+        private readonly int _itemPromoCount;
+        private readonly decimal _itemPromoPrice;
+        private readonly List<string> _lstProduct;
+
+        public MixedItemPricePromoRule(List<string> lstProduct, int promoPrice, int productCount)
+        {
+            this._lstProduct = lstProduct;
+            this._itemPromoPrice = promoPrice;
+            this._itemPromoCount = productCount;
+        }
         public decimal CalculatePromoPrice(ShoppingCart cart)
         {
-            return 0.0m;
+            var listItems = cart.CartItems.Where(c => c.Product.Equals(this._itemPromoCount) && this._lstProduct.Any(l => c.Product.ID.Equals(l.ToString())));
+            decimal TotalItemPrice = 0.0m;
+
+            if (listItems.Count() == this._lstProduct.Count)
+            {
+                TotalItemPrice += this._itemPromoPrice * this._itemPromoCount;
+                foreach (var item in listItems)
+                    item.IsRuleApplied = true;
+            }
+            return TotalItemPrice;
         }
     }
 }
