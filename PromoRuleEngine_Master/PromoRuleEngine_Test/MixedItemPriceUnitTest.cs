400527e009ff2033f62170a3cb2badaefcba8e92
rupeshmfs <rupeshkumar.nayak@gmail.com>
2020-06-07 16:58:49 +0530

    code changes to add the missed unit test

--- PromoRuleEngine_Master/PromoRuleEngine_Test/MixedItemPriceUnitTest.cs
+++ PromoRuleEngine_Master/PromoRuleEngine_Test/MixedItemPriceUnitTest.cs
@@ -12,7 +12,7 @@ using System.Threading.Tasks;
 namespace PromoRuleEngine_Test
 {
     [TestClass]
-    class MixedItemPriceUnitTest
+    public class MixedItemPriceUnitTest
     {
         private ITotalPriceCalculator _priceCalculator;
 
